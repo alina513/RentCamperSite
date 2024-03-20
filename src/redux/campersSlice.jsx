bfe5a86907902a366a7f19df73304db054633ba6
alina513 <130853290+alina513@users.noreply.github.com>
2024-03-20 14:37:06 +0200

    reducer

---
+++ src/redux/campersSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchCampers } from "./operations";
+
+const campersInitialstate = {
+    campers: [],
+    isLoading: false,
+  error: null,
+};
+
+const campersSlise = createSlice ({
+    name: 'campers',
+    initialState: campersInitialstate,
+    extraReducers: builder =>{
+        builder
+        .addCase(fetchCampers.pending, state => {
+            state.isLoading = true;
+        })
+        .addCase(fetchCampers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.error = null;
+            state.campers = action.payload;
+        })
+        .addCase(fetchCampers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        })
+    }
+})
+
+const campersReducer = campersSlise.reducer;
+export default campersReducer;
---
+++ src/redux/operations.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://65facc873909a9a65b1b9181.mockapi.io';
+
+export const fetchCampers = createAsyncThunk(
+  'campers/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/adverts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
