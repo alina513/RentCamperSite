6975856989e35f435285b53f62c174298c7421c0
alina513 <130853290+alina513@users.noreply.github.com>
2024-04-26 16:56:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/camper">
+        <BrowserRouter basename="/RentCamperSite">
           <App />
         </BrowserRouter>
       </ThemeProvider>
