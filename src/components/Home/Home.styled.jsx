b8a5db402af96f5c27266aff3e4ad1a8ef1cf308
alina513 <130853290+alina513@users.noreply.github.com>
2024-04-26 23:51:16 +0300

    Change BackgroundImage

--- src/components/Home/Home.styled.jsx
+++ src/components/Home/Home.styled.jsx
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 import bg from '../../../src/img/bg.jpg';
 
 export const Container = styled.div`
-  padding: 30px 100px 30px 100px;
   display: flex;
-  flex-direction: column;
-  text-align: center;
+  align-items: center;
+  justify-content: center;
   gap: 100px;
 
   background-image: url(${bg});
@@ -16,7 +15,5 @@ export const Container = styled.div`
 `;
 
 export const Title = styled.h1`
-  text-align: justify;
   color: white;
-  background-color: rgba(105, 105, 105, 0.5);
 `;
